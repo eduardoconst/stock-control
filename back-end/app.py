30ff3f4b45aecd38e143f7f6cab0a83be569a01d
Eduardo <eduardoconstantino2003@gmail.com>
2025-01-02 11:42:11 -0300

    start back-end

---
+++ back-end/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+from products.routes import products_api
+
+app = Flask(__name__)
+CORS(app)  # Permite chamadas de outras origens (ex.: Next.js)
+
+# Registrar rotas
+app.register_blueprint(products_api, url_prefix='/api/products')
+
+@app.route('/')
+def home():
+    return {"message": "API do Gerenciador de Estoque está no ar!"}
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ back-end/products/routes.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, jsonify, request
+
+products_api = Blueprint('products_api', __name__)
+
+# Dados simulados
+products = [
+    {"id": 1, "name": "Produto A", "stock": 10},
+    {"id": 2, "name": "Produto B", "stock": 20},
+]
+
+@products_api.route('/', methods=['GET'])
+def get_products():
+    return jsonify(products)
+
+@products_api.route('/', methods=['POST'])
+def add_product():
+    data = request.json
+    new_product = {
+        "id": len(products) + 1,
+        "name": data['name'],
+        "stock": data['stock']
+    }
+    products.append(new_product)
+    return jsonify(new_product), 201

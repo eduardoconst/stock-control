891888dbd6321504c016072848da61d0314b90bd
Eduardo <eduardoconstantino2003@gmail.com>
2025-01-02 09:11:22 -0300

    troquei de tecnologia

--- backend/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_cors import CORS
-
-def create_app():
-    app = Flask(__name__)
-    CORS(app)  # Permitir conexão com o frontend
-    from .routes import main_routes
-    app.register_blueprint(main_routes)
-    return app
